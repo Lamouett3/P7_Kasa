e350191a151ef9c8bcca88df6389f1403f867232
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-03-24 19:08:54 +0100

    header

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -7,3 +7,20 @@ function App() {
 }
 
 export default App
+
+/*
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
+      </div>
+    </BrowserRouter>
+  );
+}
+*/
