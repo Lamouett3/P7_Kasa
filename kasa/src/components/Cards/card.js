586a2d7743cadc82311a12f1ffe93cd254f39334
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-03-26 16:28:30 +0200

    before fusion

--- kasa/src/components/Cards/card.js
+++ kasa/src/components/Cards/card.js
@@ -1,18 +1,26 @@
-import data from '../../logement.JSON';
+import { useState, useEffect } from 'react';
 import './card.css';
 
 const CardsSection = () => {
-  return (
-    <div className="cards-section">
-      {data.map((card) => (
-        <div className="card" key={card.id}>
-          <img src={card.cover} alt={card.title} />
-          <h2>{card.title}</h2>
-          <p>{card.description}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default CardsSection;
+    const [data, setData] = useState([]);
+  
+    useEffect(() => {
+      fetch('../../logement.json')
+        .then(response => response.json())
+        .then(data => setData(data));
+    }, []);
+  
+    return (
+      <div className="cards-section">
+        {data.forEach((card) => (
+          <div className="card" key={card.id}>
+            <img src={card.cover} alt={card.title} />
+            <h2>{card.title}</h2>
+            <p>{card.description}</p>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  
+  export default CardsSection;
