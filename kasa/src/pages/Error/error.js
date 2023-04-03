7f66169ebff063049e2540017261283818b96855
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-04-03 02:09:06 +0200

    css issue

--- kasa/src/pages/Error/error.js
+++ kasa/src/pages/Error/error.js
@@ -1,11 +1,19 @@
 import './error.css';
 
 function Error() {
+
+  const handleClick = () => {
+    window.location.href = '/';
+  }
+
   return (
     <div className="error-container">
       <div className="error-text">
         <h1>404</h1>
         <p>Oups! La page que vous demandez n'existe pas.</p>
+        <div className="error__home">
+        <a href="/" onClick={handleClick}>Retourner sur la page d’accueil</a>
+        </div>
       </div>
     </div>
   );
