b88139cfb96b718f22b05fe93b3390c922907308
AITMESSAOUD <driss@MacBook-Pro-de-AITMESSAOUD.local>
2023-04-06 08:11:32 +0200

    correction carrousel img

--- kasa/src/components/Carrousel/carrousel.js
+++ kasa/src/components/Carrousel/carrousel.js
@@ -2,39 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import './carrousel.css';
+import { useParams } from "react-router-dom";
 
 function Carrousel() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({});
   const [currentIndex, setCurrentIndex] = useState(0);
+  const { id } = useParams();
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('../../logement.json');
-      const jsonData = await response.json();
-      setData(jsonData);
-    };
-    fetchData();
-  }, []);
+    fetch('../../logement.json')
+      .then(response => response.json())
+      .then(data => data.find(item => item.id === id))
+      .then(data => setData(data));
+  }, [id]);
 
   const handleNextClick = () => {
-    setCurrentIndex((currentIndex + 1) % data.length);
+    setCurrentIndex((currentIndex + 1) % data.pictures.length);
   };
 
   const handlePrevClick = () => {
-    setCurrentIndex((currentIndex - 1 + data.length) % data.length);
+    setCurrentIndex((currentIndex - 1 + data.pictures.length) % data.pictures.length);
   };
+  
   return (
     <div className="carrousel">
-      <img src={data[currentIndex]?.cover} alt={data[currentIndex]?.title} />
+      <img src={data.pictures && data.pictures[currentIndex]} alt={data.title} />
       <button className="carrousel__button carrousel__button--prev" onClick={handlePrevClick}>
-      <FontAwesomeIcon icon={faChevronLeft} />
+        <FontAwesomeIcon icon={faChevronLeft} />
       </button>
       <button className="carrousel__button carrousel__button--next" onClick={handleNextClick}>
-     <FontAwesomeIcon icon={faChevronRight} />
+        <FontAwesomeIcon icon={faChevronRight} />
       </button>
     </div>
   );
 }
-
 
 export default Carrousel;
