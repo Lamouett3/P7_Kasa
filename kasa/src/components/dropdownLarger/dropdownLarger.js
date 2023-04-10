371dd95364dd90a975f792e016506db079debdab
AITMESSAOUD <driss@MBPdeAIMESSAOUD.lan>
2023-04-11 00:35:24 +0200

    work on props for drodownlarger component

--- kasa/src/components/dropdownLarger/dropdownLarger.js
+++ kasa/src/components/dropdownLarger/dropdownLarger.js
@@ -1,25 +1,87 @@
 import React, { useState } from "react";
 import "./dropdownLarger.css";
 
-function DropdownLarger() {
-  const [isOpen, setIsOpen] = useState(false);
+function AboutBanner() {
+  const [isOpenFiabilite, setIsOpenFiabilite] = useState(false);
+  const [isOpenRespect, setIsOpenRespect] = useState(false);
+  const [isOpenService, setIsOpenService] = useState(false);
+  const [isOpenSecurite, setIsOpenSecurite] = useState(false);
 
   return (
-    <div className="dropdown_droplarge">
-      <button className="dropdown__button" onClick={() => setIsOpen(!isOpen)}>
-        <h4>Fiabilité</h4>
-      </button>
-      {isOpen && (
-        <div className="dropdown__content">
-          <ul className="dropdown__list">
-            <li className="dropdown__list-item">
-              <p>Fiabilité</p>
-            </li>
-          </ul>
-        </div>
-      )}
+    <div>
+      <div className="dropdown_droplarge">
+        <button
+          className="dropdownLarger__button"
+          onClick={() => setIsOpenFiabilite(!isOpenFiabilite)}
+        >
+          <h4>Fiabilité</h4>
+        </button>
+        {isOpenFiabilite && (
+          <div className="dropdownLarger__content">
+            <ul className="dropdownLarger__list">
+              <li className="dropdownLarger__list-item">
+                <p>Fiabilité</p>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <div className="dropdown_droplarge">
+        <button
+          className="dropdownLarger__button"
+          onClick={() => setIsOpenRespect(!isOpenRespect)}
+        >
+          <h4>Respect</h4>
+        </button>
+        {isOpenRespect && (
+          <div className="dropdownLarger__content">
+            <ul className="dropdownLarger__list">
+              <li className="dropdownLarger__list-item">
+                <p>Respect</p>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <div className="dropdown_droplarge">
+        <button
+          className="dropdownLarger__button"
+          onClick={() => setIsOpenService(!isOpenService)}
+        >
+          <h4>Service</h4>
+        </button>
+        {isOpenService && (
+          <div className="dropdownLarger__content">
+            <ul className="dropdownLarger__list">
+              <li className="dropdownLarger__list-item">
+                <p>Service</p>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+
+      <div className="dropdown_droplarge">
+        <button
+          className="dropdownLarger__button"
+          onClick={() => setIsOpenSecurite(!isOpenSecurite)}
+        >
+          <h4>Sécurité</h4>
+        </button>
+        {isOpenSecurite && (
+          <div className="dropdownLarger__content">
+            <ul className="dropdownLarger__list">
+              <li className="dropdownLarger__list-item">
+                <p>Sécurité</p>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
-export default DropdownLarger;
+export default AboutBanner;
